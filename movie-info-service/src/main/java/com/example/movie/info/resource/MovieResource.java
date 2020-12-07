b110188cc0858abaa6be959835edc45b9c8eb151
Abhishek Raina <abhishek.raina@tadigital.com>
2020-12-07 19:50:54 +0530

    Initial

---
+++ movie-info-service/src/main/java/com/example/movie/info/resource/MovieResource.java
@@ -0,0 +1,17 @@
+package com.example.movie.info.resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.movie.info.model.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Winter Soldier");
+	}
+}
---
+++ ratings-data-service/src/main/java/com/example/ratings/resource/RatingsResource.java
@@ -0,0 +1,18 @@
+package com.example.ratings.resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ratings.model.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+}
