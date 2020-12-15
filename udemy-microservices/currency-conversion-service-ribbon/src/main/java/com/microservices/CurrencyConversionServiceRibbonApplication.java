8f46d2c2a0f8a22e59f3b318d6230121d4e47b7f
Abhishek Raina <abhishek.raina@tadigital.com>
2020-12-15 19:08:49 +0530

    Added zuul gateway

--- udemy-microservices/currency-conversion-service-ribbon/src/main/java/com/microservices/CurrencyConversionServiceRibbonApplication.java
+++ udemy-microservices/currency-conversion-service-ribbon/src/main/java/com/microservices/CurrencyConversionServiceRibbonApplication.java
@@ -3,11 +3,13 @@ package com.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.microservices.proxy")
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class CurrencyConversionServiceRibbonApplication {
 
 	public static void main(String[] args) {
--- udemy-microservices/currency-conversion-service-ribbon/src/main/java/com/microservices/proxy/CurrencyExchangeProxy.java
+++ udemy-microservices/currency-conversion-service-ribbon/src/main/java/com/microservices/proxy/CurrencyExchangeProxy.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservices.model.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service")
+
+/**
+ * Below annotations are disabled after enabling zuul gateway
+ * otherwise use these annotations when normal feign + eureka is configured
+ * @FeignClient(name = "currency-exchange-service")
+ * @GetMapping("/currency-exchange/from/{from}/to/{to}")
+ * */
+@FeignClient(name="netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean convertCurrency(
 			@PathVariable String from, 
 			@PathVariable String to);
